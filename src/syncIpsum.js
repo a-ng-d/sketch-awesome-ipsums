7d6c7467cdffc883e3bb085edf96e1c13a48bddb
Aurélien Grimaud <a.grimaud@involt.io>
2021-12-09 22:55:54 +0100

    Add an input to get the speadsheet's sheet name

--- src/syncIpsum.js
+++ src/syncIpsum.js
@@ -1,26 +1,41 @@
 import sketch from 'sketch';
 import {getPreference, savePreference, pluginCache} from './index';
-console.log(getPreference('spreadsheetID'));
-
-sketch.UI.getInputFromUser(
-  "Sync. your awesome ipsums from your Google spreadsheet",
-  {
-    description: 'Get collaborative! First publish on the Web (File > Publish on the Web), then copy the URL and paste it below 👇',
-    numberOfLines: 3,
-    initialValue: alreadySync()
-  },
-  (err, value) => {
-    var google = validateURL();
-    if (err || value.indexOf(google) == -1) {
-      return sketch.UI.alert('Houston, there\'s a problem 😓', 'Please, try again.')
     } else {
+export default function syncIpsum() {
+  sketch.UI.getInputFromUser(
+    "Sync. your awesome ipsums from your Google spreadsheet (1/2)",
+    {
+      description: 'First publish on the Web (File > Publish on the Web), then copy the URL and paste it below 👇',
+      numberOfLines: 3,
+      initialValue: alreadySync()
+    },
+    (err, value) => {
+      var google = validateURL();
+      if (err || value.indexOf(google) == -1) {
+        return sketch.UI.alert('Houston, there\'s a problem 😓', 'Please, try again.')
+      } else {
         var ID = getID(value);
         savePreference('spreadsheetID', ID);
-        sketch.UI.message('The ipsums are on the track! 🔥');
-        console.log(getPreference('spreadsheetID'))
+        sketch.UI.getInputFromUser(
+          "Sync. your awesome ipsums from your Google spreadsheet (2/2)",
+          {
+            description: 'Type the name of the ipsums sheet (⚠️ case sensitive)',
+            numberOfLines: 1,
+            initialValue: getPreference('sheetName')
+          },
+          (err, value) => {
+            if (err) {
+              return sketch.UI.alert('Houston, there\'s a problem 😓', 'Please, try again.')
+            } else {
+              savePreference('sheetName', value);
+              sketch.UI.message('The ipsums are on the track! 🔥')
+            }
+          }
+        )
+      }
     }
-  }
-)
+  )
+}
 
 function getID(input) {
 
